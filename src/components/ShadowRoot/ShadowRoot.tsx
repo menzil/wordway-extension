29fb3f8a31105d67feb73bcccd161c5e14cdc33c
JianyingLi <lijy91@foxmail.com>
2020-01-02 23:08:55 +0800

    Remove @duik/icon

--- src/components/ShadowRoot/ShadowRoot.tsx
+++ src/components/ShadowRoot/ShadowRoot.tsx
@@ -3,10 +3,6 @@ import root from "react-shadow";
 
 // eslint-disable-next-line
 import duikItStyles from "!!raw-loader!./duik_it.txt";
-// eslint-disable-next-line
-import duikIconStyles from "!!raw-loader!./duik_icon.txt";
-
-import r from '../../utils/r';
 
 interface ShadowRootProps {
   children: any;
@@ -19,27 +15,6 @@ class ShadowRoot extends React.Component<ShadowRootProps, ShadowRootState> {
     const { children } = this.props;
     return (
       <>
-        <style>
-          {`
-          @font-face {
-            font-family: "uikon";
-            src: url(${r('/static/media/23a84f3596ea8ce60021f7540a422348.eot')});
-            src: url(${r('/static/media/23a84f3596ea8ce60021f7540a422348.eot#iefix')}) format("embedded-opentype"),
-              url(${r('/static/media/182fadb541cdfa9b77620d8b4ea81926.ttf')}) format("truetype"),
-              url(${r('/static/media/3ec057358075821566e03cdf079e3c56.woff')}) format("woff"),
-              url(${r('/static/media/934c3c35cec81863f3598db8e7c0c0bc.svg#uikon')}) format("svg");
-            font-weight: normal;
-            font-style: normal;
-          }
-          `}
-        </style>
-        <style>
-          {`
-          :root {
-            font-size: 16px;
-          }
-          `}
-        </style>
         <root.div>
           <>
             <html
@@ -50,7 +25,6 @@ class ShadowRoot extends React.Component<ShadowRootProps, ShadowRootState> {
             >
               <head>
                 <style>{duikItStyles}</style>
-                <style>{duikIconStyles}</style>
               </head>
               <body>{children}</body>
             </html>
